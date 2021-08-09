54ce708ff8658bf3b27c1fc8aff1b7151e564930
Diorditsa Vlad <doctorfat13@gmail.com>
2021-08-09 08:27:18 +0300

    commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
-dd;
+const categoriesEl = document.querySelectorAll("#categories li.item");
+console.log(`В списке ${categoriesEl.length} категории.`);
+
+categoriesEl.forEach((categoryEl) => {
+  console.log(`Категория : ${categoryEl.firstElementChild.textContent}`);
+  console.log(
+    `Количество элементов : ${categoryEl.querySelectorAll(`li`).length}`
+  );
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
+const ingredientsEl = document.querySelector("#ingredients");
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
+
+const createIngridientsEl = (ingredient) => {
+  const ingredientsElementLi = document.createElement(`li`);
+  ingredientsElementLi.textContent = ingredient;
+  return ingredientsElementLi;
+};
+
+ingredientsEl.append(...ingredients.map(createIngridientsEl));
